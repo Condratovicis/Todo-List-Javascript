880263fbbc79f938a7df0967ab28c94a758b3c2e
Joao Condratovicis <pedrocfreire@hotmail.com>
2024-05-07 19:53:58 -0300

    edit todo (JS)

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const editForm =  document.querySelector("#todo-edit_form");
 const editInput =  document.querySelector("#edit-input_form");
 const cancelEditBtn =  document.querySelector("#cancel-edit-btn");
 
+let oldInputValue;
+
 // Funções
 
 const saveTodo = (text) => {
@@ -33,7 +35,29 @@ const saveTodo = (text) => {
    todo.appendChild(removeBtn);
 
    todoList.appendChild(todo);
-   todoInput.value = " "
+   todoInput.value = " ";
+   todoInput.focus();
+};
+
+const toggleForms = () => {
+   editForm.classList.toggle("hide");
+   todoForm.classList.toggle("hide");
+   todoList.classList.toggle("hide");
+}
+
+const updateTodoTitle = (text) => {
+
+   const todos = document.querySelectorAll(".todo");
+
+   todos.forEach((todo) => {
+
+      let todoTitle = todo.querySelector("h3");
+
+      if(todoTitle.innerText === oldInputValue){
+         todoTitle.innerText = text;
+      };
+   })
+
 };
 
 // Eventos 
@@ -46,4 +70,51 @@ todoForm.addEventListener("submit", (e) => {
    if(inputValue){
       saveTodo(inputValue);
    }
-})
+});
+
+document.addEventListener("click", (e) => {
+
+   const targetEl = e.target;
+   const parentEl = targetEl.closest("div");
+   let todoTitle;
+
+   if(parentEl && parentEl.querySelector("h3")){
+      todoTitle = parentEl.querySelector("h3").innerText;
+   }
+
+   if(targetEl.classList.contains("finish-todo")){
+      // console.log("click".repeat(2));
+      parentEl.classList.toggle("done");
+   };
+
+   if(targetEl.classList.contains("remove-todo")){
+      parentEl.remove();
+   };
+
+   if(targetEl.classList.contains("edit-todo")){
+      toggleForms();
+
+      editInput.value = todoTitle;
+      oldInputValue = todoTitle;
+   };
+});
+
+cancelEditBtn.addEventListener("click", (e) => {
+   e.preventDefault();
+
+   toggleForms();
+});
+
+editForm.addEventListener("submit", (e) => {
+   e.preventDefault();
+
+   const editInputValue = editInput.value;
+
+   if(editInputValue){
+      // Atualizar
+      updateTodoTitle(editInputValue);
+   }
+
+   toggleForms();
+
+});
